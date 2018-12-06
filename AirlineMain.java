4e6071b6bc57f0c24207a9eca8373feafc384ba3
Greg <Greg@192.168.0.105>
2018-12-05 23:43:24 -0500

    Implemented route adding feature

--- AirlineMain.java
+++ AirlineMain.java
@@ -2,6 +2,7 @@ import java.io.File;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
@@ -9,6 +10,7 @@ import java.util.Scanner;
 public class AirlineMain{
 	private EdgeWeightedGraph graph;
 	private String[] citylookup;
+	private HashMap<String,Integer> cityidtable= new HashMap<String,Integer>();
 	private ArrayList<Edge> edges = new ArrayList<Edge>();
 	
 	public AirlineMain(String s) {
@@ -31,9 +33,13 @@ public class AirlineMain{
 				vertices = Integer.parseInt(read.nextLine());
 			graph = new EdgeWeightedGraph(vertices);
 			citylookup=new String[vertices];
-			for(int i=0;i<vertices;i++)
-				if(read.hasNextLine()) citylookup[i]=read.nextLine();
-			while(true) {
+			for(int i=0;i<vertices;i++) {
+				if(read.hasNextLine()) {
+					String s = read.nextLine();
+					citylookup[i]= s;
+					cityidtable.put(s, i);
+				}
+			}while(true) {
 				if(!read.hasNextInt()) break;
 				int v = read.nextInt()-1;
 				int w = read.nextInt()-1;
@@ -80,6 +86,29 @@ public class AirlineMain{
 	}
 	
 	/**
+	 * Adds a new edge to the graph between two existing vertices
+	 * 
+	 * @param city1	   vertice one
+	 * @param city2	   vertice two
+	 * @param distance distance weight of edge
+	 * @param price    price weight of edge
+	 */
+	public void addRoute(String city1, String city2, int distance, double price) {
+		int v=0,w =0;
+		try{
+			v = cityidtable.get(city1);
+			w = cityidtable.get(city2);
+		}catch(NullPointerException e) {
+			System.out.println("\nOne of the two cities entered is not valid, please check capitalization and spelling");
+			return;
+		}	
+		Edge e = new Edge(v,w,distance,price);
+		edges.add(e);
+		graph.addEdge(e);
+		System.out.println("\nAdded route of " + distance + " miles from " + city1 + " to " + city2 + " for $" + price);
+	}
+	
+	/**
 	 * Prints out the graph in the form: City1 to City2 is distance miles for $price
 	 */
 	public void printGraph() {
@@ -91,5 +120,6 @@ public class AirlineMain{
 		AirlineMain temp = new AirlineMain("a5data1.txt");
 		temp.printGraph();
 		temp.minimumSpanningTree();
+		temp.addRoute("Pittsburgh", "altoona", 30, 75.00);
 	}
 }
