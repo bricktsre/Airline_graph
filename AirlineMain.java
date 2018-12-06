89e7a8febd3485af20d823548fdadf10c287c26c
Greg <Greg@192.168.0.105>
2018-12-05 23:22:51 -0500

    Implemented the minimum spanning tree feature

--- AirlineMain.java
+++ AirlineMain.java
@@ -1,6 +1,9 @@
 import java.io.File;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class AirlineMain{
@@ -47,6 +50,36 @@ public class AirlineMain{
 	}
 
 	/**
+	 * Creates a minimum spanning tree of the graph using Kruskal's algorithm
+	 * based on distance weight of the edges
+	 */
+	public void minimumSpanningTree() {
+		LinkedList<Edge> mst = new LinkedList<Edge>();
+		double weight=0.0;
+		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
+        for (Edge e : graph.edges()) {
+            pq.add(e);
+        }
+
+        // run greedy algorithm
+        UF uf = new UF(graph.V());
+        while (!pq.isEmpty() && mst.size() < graph.V() - 1) {
+            Edge e = pq.poll();
+            int v = e.either();
+            int w = e.other(v);
+            if (!uf.connected(v, w)) { // v-w does not create a cycle
+                uf.union(v, w);  // merge v and w components
+                mst.offer(e);  // add edge e to mst
+                weight += e.getDistance();
+            }
+        }
+        System.out.println("\nThe edges in the MST based on distance follow:");
+        for(Edge e: mst) {
+        	System.out.println(citylookup[e.either()] + "," + citylookup[e.other(e.either())] + " : " + e.getDistance());
+        }
+	}
+	
+	/**
 	 * Prints out the graph in the form: City1 to City2 is distance miles for $price
 	 */
 	public void printGraph() {
@@ -57,5 +90,6 @@ public class AirlineMain{
 	public static void main(String[] args){
 		AirlineMain temp = new AirlineMain("a5data1.txt");
 		temp.printGraph();
+		temp.minimumSpanningTree();
 	}
 }
